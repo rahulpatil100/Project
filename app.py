cdf5411ecbe4fab34e4f291cd72cb71011dd4ecf
Rahul Patil <rahulcs629@gmail.com>
2018-07-19 20:12:16 +0530

    Commited Succesfully

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+"""PYTHON """
 # a = "Hello World"
 # print(a.replace('H', 'J' ))
 
-#split mehtod
+"""split mehtod"""
 # a = "hello World"
 # print(a.split(","))
 
-#user input
+"""user input"""
 # print("Enter your name:")
 # x = input()
 # print(" Hello " +x)
 
-
+"""Addition of two no """
 # print("Addition of two variable")
 #
 # num1 = float(input("Please Enter first number : "))
@@ -18,29 +19,29 @@
 # sum = float(num1) + float(num2)
 # print('The total of :',sum)
 
-#Change list
+"""Change list"""
 # list = ['Apple', 'Banana', 'Cherry']
 # list[2] = 'Mango'
 # print(list)
 
-#Constructor under list
+"""Constructor under list"""
 # thislist = list(('apple','banana','mango')) #note double bracket use constructor
 # print(thislist)
 
 
-#using append() mehtod to add list item
+"""using append() mehtod to add list item"""
 # thislist = list(('Linux','Windows','Unix'))
 # thislist.append('My love is MAC')
 # print(thislist)
 
-#using remove() method to remove item into list
+"""using remove() method to remove item into list"""
 
 # thislist = list(('Linux','Windows','My First Love Mac'))
 # # thislist.remove(thislist[1])
 # # print(thislist)
 
 
-#using len() method to to see length of  list
+"""using len() method to to see length of  list"""
 
 # thislist = list(('Linux','Windows','Apple'))
 # thislist=(len(thislist))
@@ -60,52 +61,52 @@
 # reverse()	Reverses the order of the list
 # sort()	Sorts the list
 
-#using sorted list
+"""using sorted list"""
 # thislist = list(('Windows','Linux','Apple'))
 # thislist = (sorted(thislist))
 # print(thislist)
 
 
-#Tuple is collection of ordered but Tuple is unchangable & Tuple  is wrriten into round bracket
-#Tuple is similar to list
-
+"""Tuple is collection of ordered but Tuple is unchangable & Tuple  is wrriten into round bracket
+Tuple is similar to list
+"""
 # thisTuple = ('Windows','My love MacOs Sierra','Linux')
 # print(thisTuple)
 
-#You canot change Tuple
+"""You canot change Tuple"""
 # thisTuple = ('Windows','My love MacOs Sierra','Linux')
 # thisTuple[3]='Unix' #test changabilitly
 # print(thisTuple)
 
 
-# using Tuple Constructor use (()) bracket
+""" using Tuple Constructor use (()) bracket"""
 # thisTuple = tuple (('Windows','My Love MACOS Sierra','Linux'))
 # print(thisTuple)
 
-# #using Tuple constructor lenth of tuple
+"""using Tuple constructor lenth of tuple"""
 # thisTuple = tuple(('Windows','My Love MacOs Sierra','Unix'))
 # thisTuple=(len(thisTuple))
 # print(thisTuple)
 
 
 
-#Set is collection of unordered and unindexed. In python set is are written in python curly brackets
+"""Set is collection of unordered and unindexed. In python set is are written in python curly brackets"""
 
 # thisSet = {'Windows','Mac','Linux'}
 # print(thisSet)
 #the set list is unordered, so the items will appear in a random order.
 
 
-#Set() using constructor
+"""Set() using constructor"""
 # thisSet = (('windows','Mac','Linux'))
 # print(thisSet)
 
-#using add() mehtod Set
+"""using add() mehtod Set"""
 # thisSet = set(('Windows','Mac','Linux','unix'))
 # thisSet.add('Apple dude')
 # print(thisSet)
 
-#Set using remove() method
+"""Set using remove() method"""
 # thisSet = set(('Windows','Mac','Ubantu','linux'))
 # thisSet.remove('Ubantu')
 # print(thisSet)
