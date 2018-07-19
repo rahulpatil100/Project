aea7ae210d9b9588c1b7bcd952e7fffecb925021
Rahul Patil <rahulcs629@gmail.com>
2018-07-19 16:08:04 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+# a = "Hello World"
+# print(a.replace('H', 'J' ))
+
+#split mehtod
+# a = "hello World"
+# print(a.split(","))
+
+#user input
+# print("Enter your name:")
+# x = input()
+# print(" Hello " +x)
+
+
+# print("Addition of two variable")
+#
+# num1 = float(input("Please Enter first number : "))
+# num2 = float(input("Please Enter secound number :"))
+# sum = float(num1) + float(num2)
+# print('The total of :',sum)
+
+#Change list
+# list = ['Apple', 'Banana', 'Cherry']
+# list[2] = 'Mango'
+# print(list)
+
+#Constructor under list
+# thislist = list(('apple','banana','mango')) #note double bracket use constructor
+# print(thislist)
+
+
+#using append() mehtod to add list item
+# thislist = list(('Linux','Windows','Unix'))
+# thislist.append('My love is MAC')
+# print(thislist)
+
+
